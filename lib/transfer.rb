1c70fe868c3d7765ab1e75fc7203280628922515
Daniel Etter <etter.dt@gmail.com>
2018-08-09 18:44:40 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
 
+  attr_reader :sender, :receiver
+  attr_accessor :status
+  
+  def initialize(sender, receiver)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+  end
 end
